172009a4ba9c21b920e2cdfbd6fcf3bbd9878ad8
bennetel <33488288+bennetel@users.noreply.github.com>
2017-11-15 17:11:05 +0100

    Hands IO
    skrrra

---
+++ Assets/LeapMotion/Core/Scripts/toggleHands.cs
@@ -0,0 +1,30 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleHands : MonoBehaviour {
+
+    HandPool _handpool;
+	// Use this for initialization
+	void Start () {
+        _handpool = this.GetComponent<HandPool>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.O))
+        {   
+            _handpool.DisableGroup("HandCapsules1");
+            _handpool.DisableGroup("HandRidgids1");
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _handpool.EnableGroup("HandCapsules1");
+            _handpool.EnableGroup("HandRidgids1");
+
+        }
+    }
+}
